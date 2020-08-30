49339af467ed22c546c9b1074fad6a6ce86fdce9
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2020-08-30 00:20:03 -0700

    Water plane starting to look like something. Still broken

--- src/shaders/WaterShader/vertex.glsl
+++ src/shaders/WaterShader/vertex.glsl
@@ -1,5 +1,10 @@
 uniform float frame;
 
+uniform vec3 nearLeft;
+uniform vec3 nearRight;
+uniform vec3 farLeft;
+uniform vec3 farRight;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec4 vWorldPos;
@@ -7,9 +12,13 @@ varying vec4 vWorldPos;
 
 void main() {
     vUv = uv;
-    vec4 worldPos = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    worldPos.y += 50.0*sin(worldPos.x/80.0+frame/30.0);
-    vNormal = normalize(vec3(1.0, cos(worldPos.x/80.0+frame/30.0), 0.0));
+    vec3 nearInterpolated = mix(nearLeft, nearRight, uv.x);
+    vec3 farInterpolated = mix(farLeft, farRight, uv.x);
+    vec4 interpolatedPos = vec4(mix(nearInterpolated, farInterpolated, uv.y), 1.0);
+
+    vec4 worldPos = projectionMatrix * modelViewMatrix * interpolatedPos;
+    worldPos.y += 5.0*sin(worldPos.x/80.0+frame/30.0);
+    vNormal = normalize(vec3(1.0, cos(worldPos.y/80.0+frame/30.0), 0.0));
     vWorldPos = worldPos;
     gl_Position =  worldPos;
 }
--- src/splishSplosh.js
+++ src/splishSplosh.js
@@ -2,7 +2,7 @@
   class splishSplosh extends NIN.THREENode {
     constructor(id, options) {
       super(id, {
-        camera: options.camera,
+        camera: new THREE.PerspectiveCamera(45, 16/9, 0.1, 1000),
         outputs: {
           render: new NIN.TextureOutput()
         }
@@ -13,12 +13,16 @@
 
       const shader = SHADERS["WaterShader"];
 
-      this.water = new THREE.Mesh(new THREE.PlaneGeometry(1, 1,100,100),
-                                  new THREE.ShaderMaterial(shader).clone());
-      this.uniforms;
+      // Water plane is the XZ plane, used for culling calculations
+      this.waterPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0));
+      let mat = new THREE.ShaderMaterial(shader)
+      //mat.wireframe = true;
+      this.water = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100,400),
+                                  mat.clone());
+      this.water.frustumCulled = false; //Never cull this, as the position is calculated in shader
 
-      this.water.rotation.x = -3.1415/2.0;
-      this.water.position.y = -50;
+
+
       this.ground.position.y = -50;
       this.ground.position.z = -100;
 
@@ -30,7 +34,7 @@
 
       this.textureCube = new THREE.CubeTextureLoader().load( urls );
       this.textureCube.encoding = THREE.sRGBEncoding;
-      this.scene.background = this.textureCube;
+      //this.scene.background = this.textureCube;
 
       this.scene.add(this.ground);
       this.scene.add(this.water);
@@ -39,15 +43,110 @@
       light.position.set(50, 50, 50);
       this.scene.add(light);
 
-      this.camera.position.z = 100;
+      this.camera.position.z = 50;
+      this.camera.position.y = 1050;
+      this.camera.rotation.x = -0.09;
+      this.camera.updateProjectionMatrix();
+      this.camera.updateMatrixWorld();
+    }
+
+    calcFrustumSeaIntersection(seaPlane){
+      this.camera.updateProjectionMatrix();
+      // Calculate water plane intersection with view frustum
+      let PVinv = this.camera.projectionMatrixInverse.premultiply(this.camera.matrixWorld);
+
+      let nearTopLeft = (new THREE.Vector3(-1,1,-1)).applyMatrix4(PVinv);
+      let nearTopRight = (new THREE.Vector3(1,1,-1)).applyMatrix4(PVinv);
+      let nearBottomRight = (new THREE.Vector3(1,-1,-1)).applyMatrix4(PVinv);
+      let nearBottomLeft = (new THREE.Vector3(-1,-1,-1)).applyMatrix4(PVinv);
+
+      let farTopLeft = (new THREE.Vector3(-1,1,1)).applyMatrix4(PVinv);
+      let farTopRight = (new THREE.Vector3(1,1,1)).applyMatrix4(PVinv);
+      let farBottomRight = (new THREE.Vector3(1,-1,1)).applyMatrix4(PVinv);
+      let farBottomLeft = (new THREE.Vector3(-1,-1,1)).applyMatrix4(PVinv);
+
+      /*console.log("Far top left: " + farTopLeft);
+      console.log( farTopLeft);
+      console.log("Far bottom right: " + farBottomRight);
+      console.log( farBottomRight);
+      console.log("Near top left: " + nearTopLeft);
+      console.log( nearTopLeft);
+      console.log("Near bottom right: " + nearBottomRight);
+      console.log( nearBottomRight);*/
+
+      let linesOfInterest = [
+        new THREE.Line3(nearTopLeft, farTopLeft),
+        new THREE.Line3(nearTopRight, farTopRight),
+        new THREE.Line3(nearBottomRight, farBottomRight),
+        new THREE.Line3(nearBottomLeft, farBottomLeft),
+
+        new THREE.Line3(farTopLeft, farTopRight),
+        new THREE.Line3(farTopRight, farBottomRight),
+        new THREE.Line3(farBottomRight, farBottomLeft),
+        new THREE.Line3(farBottomLeft, farTopLeft)
+      ]
+
+
+
+      //TODO: This need to be generalized for rotation etc
+      // Maybe find the 4 intersection points among all lines and sort them after?
+      
+      let intersectPoint = new THREE.Vector3();
+      let nearLeft = seaPlane.intersectLine(linesOfInterest[3], intersectPoint);;
+
+      //nearLeft.x += 2000;
+      console.log(nearLeft);
+      if(nearLeft === undefined){
+        console.log(":(");
+        console.log(linesOfInterest[3]);
+        console.log(seaPlane);
+        console.log(intersectPoint);
+        nearLeft = [-20, 0, 0]
+      }
+
+      intersectPoint = new THREE.Vector3();
+      let nearRight= seaPlane.intersectLine(linesOfInterest[2], intersectPoint);;
+      if(nearRight === undefined){
+        console.log(":(");
+        nearRight = [20, 0, 0]
+      }
+
+      intersectPoint = new THREE.Vector3();
+      let farLeft= seaPlane.intersectLine(linesOfInterest[7], intersectPoint);;
+      if(farLeft === undefined){
+        console.log(":(");
+        farLeft = [-1000, 0, -1000]
+      }
+
+      intersectPoint = new THREE.Vector3();
+      let farRight= seaPlane.intersectLine(linesOfInterest[5], intersectPoint);;
+      if(farRight === undefined){
+        console.log(":(");
+        farRight = [1000, 0, -1000]
+      }
+
+
+      //console.log([nearLeft, nearRight, farRight, farLeft]);
+
+
+  
+      return [nearLeft, nearRight, farRight, farLeft];
+
     }
 
     update(frame) {
       super.update(frame);
+
+      let intersections = this.calcFrustumSeaIntersection(this.waterPlane);
+
+      this.water.material.uniforms.nearLeft.value = intersections[0];
+      this.water.material.uniforms.nearRight.value = intersections[1];
+      this.water.material.uniforms.farRight.value  = intersections[2];
+      this.water.material.uniforms.farLeft.value   = intersections[3];
       
       // Update water uniforms
       this.water.material.uniforms.frame.value = frame;
-      this.water.material.uniforms.bottomleft.value = vec3(0,0,0);
+
 
       this.ground.rotation.x = Math.sin(frame / 50);
       this.ground.rotation.y = Math.cos(frame / 50);
